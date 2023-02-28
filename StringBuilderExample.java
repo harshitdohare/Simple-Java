9cedbbccb9abc5bd3e6f5ba91c509862a3982314
Harshit Dohare <66507614+harshitdohare@users.noreply.github.com>
2023-02-28 11:23:06 +0530

    Ques1

---
+++ StringBuilderExample.java
@@ -0,0 +1,7 @@
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+        System.out.println(sb.toString());
+    }
+}
